2c3323f87eeb9fbb77e3afb008357339663edde2
deeabdukarimova <deeabdukarimova@gmail.com>
2021-02-10 06:49:38 -0600

    methods added to Recycler View

--- app/src/main/java/com/example/registrationform/StudentsRecViewAdapter.java
+++ app/src/main/java/com/example/registrationform/StudentsRecViewAdapter.java
@@ -1,14 +1,56 @@
 package com.example.registrationform;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class StudentsRecViewAdapter {
+import java.util.ArrayList;
 
+public class StudentsRecViewAdapter extends RecyclerView.Adapter<StudentsRecViewAdapter.ViewHolder_Student> {
+    private ArrayList<StudentsContact> students=new ArrayList<>();
+    private Context context;
+
+    public StudentsRecViewAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder_Student onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /*holds and returns the View*/
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.students_list,parent,false);
+        ViewHolder_Student holder_student=new ViewHolder_Student(view);
+        return holder_student;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder_Student holder, int position) {
+        holder.txtLastName.setText(students.get(position).getLastName());
+        holder.txtName.setText(students.get(position).getName());
+        holder.txtEmail.setText(students.get(position).getEmail());
+        holder.txtGender.setText(students.get(position).getGender());
+        holder.txtCountry.setText(students.get(position).getCountry());
+
+        /*if particular student is selected, display Toast message*/
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, students.get(position).getName()+" Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return students.size();
+    }
 
 
     /*inner class within the StudentsRecViewAdapter which will hold the views */
